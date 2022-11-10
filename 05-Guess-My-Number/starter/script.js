efb5bd183040e47a99c832ebd915cebdb4127fe6
FoboIX <mills_forrest@yahoo.com>
2022-11-10 16:34:39 -0600

    working on dom manipulation - basic

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+document.querySelector('.message').textContent = '🤮 Correct';
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  //error handling
+  if (!guess) {
+    alert('Enter a real number.');
+  }
+});
