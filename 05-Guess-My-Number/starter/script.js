fb429a647cd4dff972ae6a4a755d15533de1ee22
FoboIX <mills_forrest@yahoo.com>
2022-11-10 16:58:09 -0600

    added comments

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,16 +1,18 @@
 'use strict';
 
+// generating random num between 1 - 20
 const number = Math.trunc(Math.random() * 20) + 1;
 
 document.querySelector('.number').textContent = number;
 
 console.log(number);
 
+// eventListener for 'click' on check button
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
-  //error handling
+  //error handling - displaying too high or too low on guesses
   if (!guess) {
     alert('Enter a real number.');
   } else if (guess === number) {
