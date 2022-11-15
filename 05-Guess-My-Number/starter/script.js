75773522864196b0a863f167c887b934bfb07d59
FoboIX <mills_forrest@yahoo.com>
2022-11-15 13:48:14 -0600

    implemented highscore

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,10 +1,9 @@
 'use strict';
 
 // generating random num between 1 - 20
-const number = Math.trunc(Math.random() * 20) + 1;
+let number = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-
-document.querySelector('.number').textContent = number;
+let highscore = 0;
 
 console.log(number);
 
@@ -18,8 +17,14 @@ document.querySelector('.check').addEventListener('click', function () {
     alert('Enter a real number.');
   } else if (guess === number) {
     document.querySelector('.message').textContent = '🗣 Correct Number!';
+    document.querySelector('.number').textContent = number;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
   } else if (guess > number) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📈 Too high!';
@@ -39,4 +44,16 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
+
+  document.querySelector('.again').addEventListener('click', function () {
+    score = 20;
+    number = Math.trunc(Math.random() * 20) + 1;
+
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.message').textContent = 'Start guessing...';
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+    document.querySelector('.number').style.width = '15rem';
+  });
 });
