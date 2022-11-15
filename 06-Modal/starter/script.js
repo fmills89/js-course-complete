c18fbdb6075a2d894aaef0fbe323f0ff618efc07
FoboIX <mills_forrest@yahoo.com>
2022-11-15 15:32:34 -0600

    refactored close/open modal

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -7,9 +7,22 @@ const btnOpenModal = document.querySelectorAll('.show-modal');
 
 console.log(btnOpenModal);
 
+// function to open modal
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+// DRY - function to close modal
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
 for (let i = 0; i < btnOpenModal.length; i++) {
-  btnOpenModal[i].addEventListener('click', function () {
-    console.log('Button clicked');
-    modal.classList.remove('hidden');
-  });
+  btnOpenModal[i].addEventListener('click', openModal);
 }
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
