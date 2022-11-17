1dac4212a4609df4ae4006bc99c42d371b504cd8
FoboIX <mills_forrest@yahoo.com>
2022-11-17 12:42:58 -0600

    rolling dice adding to currentScore

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -3,6 +3,10 @@
 // adding El represents a DOM element
 let score0El = document.querySelector('#score--0');
 let score1El = document.getElementById('score--1');
+
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -12,6 +16,8 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 // hiding dice w/ classlist css hidden
 diceEl.classList.add('hidden');
+
+let currentScore = 0;
 
 //rolling dice functionally
 btnRoll.addEventListener('click', function () {
@@ -24,4 +30,12 @@ btnRoll.addEventListener('click', function () {
   // 2. Display dice
   diceEl.classList.remove('hidden');
   diceEl.src = `dice-${diceRoll}.png`;
+
+  // 3. Check for rolled 1: If true, switch to next player
+  if (diceRoll !== 1) {
+    // Add dice to current score
+    currentScore += diceRoll;
+    current0El.textContent = currentScore;
+  } else {
+  }
 });
