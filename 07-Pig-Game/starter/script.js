4e568efd063ab1617eb530b752a4fcb5be327743
FoboIX <mills_forrest@yahoo.com>
2022-11-17 14:27:05 -0600

    added boolean isPlaying to prevent addl func

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -12,7 +12,7 @@ const current1El = document.getElementById('current--1');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
-const btnHold = document.querySelector('.btn-hold');
+const btnHold = document.querySelector('.btn--hold');
 
 score0El.textContent = 0;
 score1El.textContent = 0;
@@ -22,35 +22,65 @@ diceEl.classList.add('hidden');
 const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+let playing = true;
+
+// Reusable piece of code
+const switchPlayer = function () {
+  // Reassigning active player and checking if player is 0 if not
+  // activePlayer is 1, otherwise false = 0;
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  // using toggle method to remove/add class based on which
+  // player - player0 has inital class first
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
 
 //rolling dice functionally
 btnRoll.addEventListener('click', function () {
-  // 1. Generating a random dice roll
+  if (playing) {
+    // 1. Generating a random dice roll
+    let diceRoll = Math.trunc(Math.random() * 6) + 1;
 
-  let diceRoll = Math.trunc(Math.random() * 6) + 1;
+    console.log(diceRoll);
 
-  console.log(diceRoll);
+    // 2. Display dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${diceRoll}.png`;
 
-  // 2. Display dice
-  diceEl.classList.remove('hidden');
-  diceEl.src = `dice-${diceRoll}.png`;
+    // 3. Check for rolled 1: If true, switch to next player
+    if (diceRoll !== 1) {
+      // Add dice to current score
+      currentScore += diceRoll;
+      // build id name dynamiaclly
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      switchPlayer();
+    }
+  }
+});
 
-  // 3. Check for rolled 1: If true, switch to next player
-  if (diceRoll !== 1) {
-    // Add dice to current score
-    currentScore += diceRoll;
-    // build id name dynamiaclly
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-  } else {
-    // Reassigning active player and checking if player is 0 if not
-    // activePlayer is 1, otherwise false = 0;
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    // using toggle method to remove/add class based on which
-    // player - player0 has inital class first
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    // 1. Add current score to active player's score
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    // 2. Check if players score is >= 100
+    if (scores[activePlayer] >= 20) {
+      // Finish the game
+      playing = false;
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      // Switch players
+      switchPlayer();
+    }
   }
 });
